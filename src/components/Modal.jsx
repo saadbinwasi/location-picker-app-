37b21067021fec3b62c62f267c14b4aaa1f9b7eb
saad <saad.bin@saad.com>
2024-08-26 16:03:23 +0500

    done learning useEffect

---
+++ src/components/Modal.jsx
@@ -0,0 +1,25 @@
+
+import { useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
+
+function Modal({ children,open,onClose }) {
+  const dialog = useRef();
+
+  useEffect(() => {
+    if(open){
+      dialog.current.showModal();
+    }else {
+      dialog.current.close();
+    }
+  },[open])
+
+
+  return createPortal(
+    <dialog ref={dialog} className="modal" onClose={onClose}>
+      {children}
+    </dialog>,
+    document.getElementById('modal')
+  );
+};
+
+export default Modal;
